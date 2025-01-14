78656dac8d6b018b54daec3f44e3641ae0f7967d
Elder <eldergalvao07@gmail.com>
2025-01-14 16:54:08 -0300

    Complementos Dicionário

--- DictionaryPython.py
+++ DictionaryPython.py
@@ -4,7 +4,17 @@ dicionario = {'Curso':'Python para ML',
               'Produtor':'Didática Tech',
               'Preço':'Gratuito', 
               'Nota': 10}
-
 print (dicionario['Preço'])
 
+a = dicionario['Nota']
+print(a)
+
+dicionario['Pré-Requisito'] = 'Python Básico'
+print(dicionario)
+
+print(dicionario.keys())
+print(dicionario.values())
+
+dicionario.clear()
+print(dicionario)
 #Nas listas encontra o valor pelo índice, no dicionário encontra o valor pela palavra
