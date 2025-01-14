57e4813ac218fd85ed0ba2a3b99488bb83d3345c
Elder <eldergalvao07@gmail.com>
2025-01-14 16:39:05 -0300

    Trabalhando com dicionário

---
+++ DictionaryPython.py
@@ -0,0 +1,10 @@
+#dicionario = {chave:valor} 
+
+dicionario = {'Curso':'Python para ML', 
+              'Produtor':'Didática Tech',
+              'Preço':'Gratuito', 
+              'Nota': 10}
+
+print (dicionario['Preço'])
+
+#Nas listas encontra o valor pelo índice, no dicionário encontra o valor pela palavra
