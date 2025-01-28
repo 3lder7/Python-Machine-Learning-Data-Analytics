612d9328c6d353b995e3cf87b16004854cc83bfd
Elder <eldergalvao07@gmail.com>
2025-01-28 17:06:06 -0300

    Trabalhando com DataFrame em Pandas / Trabalhando com Series

--- PandasIntroducting.py
+++ PandasIntroducting.py
@@ -1,10 +1,18 @@
 import pandas as pd
+alunos = {'Nome': ['Ricardo', 'Pedro', 'Roberto', 'Carlos'], 
+          "Nota": [4, 7, 5.5, 9], 
+          "Aprovado": ['Não', 'Sim', 'Não', 'Sim']}
+dataframe = pd.DataFrame(alunos)
+print(dataframe)
 
-# Ler arquivo Excel
-dados = pd.read_excel("arquivo1.xlsx")
-print(dados.head())
+import numpy as np
+objeto1 = pd.Series([2, 6, 9, 10, 5])#vertical
+print(objeto1)
 
+array1 = np.array([2, 6, 9, 10, 5])#horizontal unidimensional
+print(array1)
+array2=np.array([(2, 6, 9, 10, 5), (8, 7, 3, 1, 4)])#horizontal bidimensional
+print(array2)
 
-# Ler o arquivo CSV
-dados2 = pd.read_csv("athlete_events.csv/athlete_events.csv")
-print(dados2.head())
+objeto2 = pd.Series(array1)#Series é um array unidimensional
+print(objeto2)
