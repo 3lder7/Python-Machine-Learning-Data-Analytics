3ba84e0e726d8507c55010e36647052703f0c404
3lder7 <eldergalvao07@gmail.com>
2025-03-14 11:55:42 -0300

    Adicionando estrutura básica do sistema bancário com opções de depósito, saque e extrato

---
+++ python-SuzanoDIO/desafioSistemaBancario.py
@@ -0,0 +1,32 @@
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3 #constante
+
+menu = """
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+while True:
+
+    opcao = input(menu)
+
+    if(opcao == "d"):
+        print("Depósito")
+
+    elif(opcao == "s"):
+        print("Saque")
+    
+    elif(opcao == "e"):
+        print("Extrato")
+    
+    elif(opcao == "q"):
+        break
+
+    else:
+        print("Opção inválida")
