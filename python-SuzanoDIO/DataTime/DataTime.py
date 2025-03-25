21cd13a8a12d4cf7df221a605f41855102ec0f80
3lder7 <eldergalvao07@gmail.com>
2025-03-25 12:47:30 -0300

    Adiciona exemplo de uso do método today e data/hora no módulo datetime

--- python-SuzanoDIO/DataTime/DataTime.py
+++ python-SuzanoDIO/DataTime/DataTime.py
@@ -2,3 +2,7 @@ from datetime import date
 d= date(2023, 7, 19)#ano mês e dia
 print(d)
 
+print(date.today())#data atual
+
+data_hora = date(2023, 7, 19, 8, 30, 0)#ano mês dia hora minuto segundo
+print(data_hora)
